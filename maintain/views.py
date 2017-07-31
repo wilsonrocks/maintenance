635cf5770a8931113f0ae5ade53db7fa8df18133
James Wilson <oh.that.wilson@googlemail.com>
2017-07-31 11:16:39 +0100

    Complete view now works (but doesn't return anywhere nice (YET)

--- maintain/views.py
+++ maintain/views.py
@@ -13,6 +13,15 @@ def all_tasks():
             jobs=models.Job.select(),
             categories=models.Category.select())
 
+@app.route("/todo")
+def todo():
+    return render_template("grouped.html",
+            jobs=models.Job.select().where(models.Job.completed == None),
+            categories=models.Category.select())
+
+
 @app.route("/complete/<id>")
 def complete(id):
-    return("Complete {}".format(id))
+    job = models.Job.get(models.Job.id==id)
+    job.complete()
+    return "Completed job #{}: {}".format(job.id,job.info)
